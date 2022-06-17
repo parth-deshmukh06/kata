5f91416979983bf606503edea4f03261a73f400a
Parth Deshmukh <parth.deshmukh@technogise.com>
2022-06-17 15:25:14 +0530

    Added Test- scoreShouldReturnZeroWhenZeroPinIsKnockedInAFrame (green)

--- src/test/java/bowling/BowlingTest.java
+++ src/test/java/bowling/BowlingTest.java
@@ -1,4 +1,20 @@
 package bowling;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class BowlingTest {
+    @Test
+    public void scoreShouldReturnZeroWhenZeroPinIsKnockedInAFrame(){
+        Game game = new Game();
+
+        for(int rolls=0;rolls<=2;rolls++){
+            game.roll(0);
+        }
+
+        assertEquals(0, game.score());
+    }
+
 }
