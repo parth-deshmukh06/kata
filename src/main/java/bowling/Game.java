9e6b8fe255f95e6025b62ff7b44e8007695a5739
Parth Deshmukh <parth.deshmukh@technogise.com>
2022-06-17 16:36:44 +0530

    Added Test- scoreShouldReturnNineteenWhenItsASpareAndThreePinsAreKnockedInEachRollOfNextFrame (green)

--- src/main/java/bowling/Game.java
+++ src/main/java/bowling/Game.java
@@ -3,11 +3,25 @@ package bowling;
 public class Game {
 
     private int score = 0;
+    private final int[] rollScore = new int[21];
+    private int currentRoll = 0;
     public void roll(int pins) {
-        score+=pins;
+        rollScore[currentRoll]=pins;
+        currentRoll++;
+        //score+=pins;
     }
 
     public int score() {
+        int i = 0;
+        for(int frames=0;frames<10;frames++){
+            if(rollScore[i] + rollScore[i+1] == 10){
+                score = 10 + rollScore[i+2];
+            }else{
+                score += rollScore[i] + rollScore[i+1];
+            }
+            i+=2;
+        }
+
         return score;
     }
 }
