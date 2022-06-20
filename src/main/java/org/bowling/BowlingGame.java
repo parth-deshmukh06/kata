73eca374baca441ad691e1fc6fbbe8aa5d7b214e
shrishti-27 <shrishtijain1345@gmail.com>
2022-06-20 12:51:55 +0530

    green test case for one spare

--- src/main/java/org/bowling/BowlingGame.java
+++ src/main/java/org/bowling/BowlingGame.java
@@ -2,16 +2,20 @@ package org.bowling;
 
 public class BowlingGame {
     private int totalScores=0;
-    private int rolls[] = new int[21];
+    private int[] rolls = new int[21];
     int currentRoll=0;
     public void roll(int numberOfKnockedPins){
         rolls[currentRoll] = numberOfKnockedPins;
         currentRoll++;
     }
     public int score() {
-        for(int i=0;i<currentRoll;i++)
+        for(int i=0;i<currentRoll;)
         {
-            totalScores+= rolls[i];
+            if ((rolls[i] + rolls[i+1])==10) {
+                totalScores+=rolls[i+2];
+            }
+            totalScores+= rolls[i]+rolls[i+1];
+            i=i+2;
         }
         return totalScores;
     }
