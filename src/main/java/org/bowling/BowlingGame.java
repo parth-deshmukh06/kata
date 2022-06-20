c870ff3fc80153352f85cb789c36721a0663f102
shrishti-27 <shrishtijain1345@gmail.com>
2022-06-20 13:05:01 +0530

    refactored test case for one spare

--- src/main/java/org/bowling/BowlingGame.java
+++ src/main/java/org/bowling/BowlingGame.java
@@ -1,21 +1,21 @@
 package org.bowling;
 
 public class BowlingGame {
-    private int totalScores=0;
     private int[] rolls = new int[21];
     int currentRoll=0;
     public void roll(int numberOfKnockedPins){
-        rolls[currentRoll] = numberOfKnockedPins;
-        currentRoll++;
+        rolls[currentRoll++] = numberOfKnockedPins;
     }
     public int score() {
-        for(int i=0;i<currentRoll;)
+        int totalScores=0;
+        int i=0;
+        for(int frame=0;frame<10; frame++)
         {
             if ((rolls[i] + rolls[i+1])==10) {
                 totalScores+=rolls[i+2];
             }
             totalScores+= rolls[i]+rolls[i+1];
-            i=i+2;
+            i+=2;
         }
         return totalScores;
     }
